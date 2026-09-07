64a0726a2b7781efdc8450a430893d33bd796f45
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-09-07 16:19:20 +0500

    Timing and Testimonial sections

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Stats from "./sections/Stats";
 import Dishes from "./sections/Dishes";
 import Features from "./sections/Features";
 import BookingProcess from "./sections/BookingProcess";
+import Timing from "./sections/Timing";
+import TestimonialSection from "./sections/TestimonialSection";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
       <Dishes />
       <Features />
       <BookingProcess />
+      <Timing />
+      <TestimonialSection />
     </>
   );
 };
